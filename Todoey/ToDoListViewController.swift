a9ebd084e04030449f6b5e6278e70626f770ac86
Oz Arie Tal Shachar <oz@h-macbook-pro-sl-oz.local>
2018-02-11 15:30:50 +0200

    Todo list complete with datasource and delegate methods

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Oz Arie Tal Shachar on 11.2.2018.
+//  Copyright © 2018 Oz Arie Tal Shachar. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let itemArray : [String] = ["Shoshana", "Damary", "Ofra Haza"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: Tableview Datasource Method
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK: Tableview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row number: \(indexPath.row), Text is: \(itemArray[indexPath.row])")
+        
+        //add checkbox to selected row
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+
+        
+        
+        //change appearance of selected row
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        <#code#>
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+}
+
